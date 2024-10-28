58278bbdd187742d70e0d577fd91cb5974b1a889
clwaltz <waltzc@msoe.edu>
2024-10-28 13:39:00 -0500

    Piece header file done

--- Piece.h
+++ Piece.h
@@ -8,9 +8,28 @@
 
 
 class Piece {
-
+    char type;
+public:
+    virtual char getType() const;
+    Piece(char type);
+    virtual ~Piece();
 };
 
+class X: public Piece {
+    char type;
+public:
+    X(): Piece('X') {}
+    char getType() const override;
+    ~X() override;
+};
+
+class O: public Piece {
+    char type;
+public:
+    O(): Piece('O') {}
+    char getType() const override;
+    ~O() override;
+};
 
 
 #endif //PIECE_H
